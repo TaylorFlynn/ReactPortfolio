0f98472a05616da62e6b33f70ee3750c4a510ac0
Taylor <97355963+TaylorFlynn@users.noreply.github.com>
2022-02-22 06:43:25 -0800

    Hex Styled Component Translation

--- src/components/HexBackground.jsx
+++ src/components/HexBackground.jsx
@@ -42,7 +42,7 @@ const BackdropOverlay = styled.div`
   background: radial-gradient(#05050500 55%, #0000008c);
   z-index: -50;
   }
-`
+`;
 
 const HexagonBox = styled.div`
   display: flex;
@@ -53,31 +53,36 @@ const HexagonBox = styled.div`
   inset: initial;
   z-index: -100;
   cursor: pointer;
-`
+  height: ${(props) => props?.HW+"px" || "60px"};
+  width: ${(props) => props?.HW+"px" || "60px"};
+  inset: "initial";
+  top: ${(props) => props.topOffset+"px"};
+  right: ${(props) => props.rightOffset};
+`;
 
 const HexagonContainer = styled.div`
   height: 100%;
   width: 57.75%;
   position: relative;
-`
+`;
 
 const HexagonInner = styled.div`
   display: flex;
   position: absolute;
   height: 100%;
   width: 100%;
-`
+`;
 
 const Hexagon = styled.div`
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.bgColor};
+  background-color: ${(props) => props?.bgColor || "rgb(29, 29, 29)"};
   position: absolute;
   border-radius: 1px;
   ::after{
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.bgColor};
+  background-color: ${(props) => props?.bgColor || "rgb(29, 29, 29)"};
   position: absolute;
   border-radius: 1px;
   content: "";
@@ -88,7 +93,7 @@ const Hexagon = styled.div`
   ::before{
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.bgColor};
+  background-color: ${(props) => props?.bgColor || "rgb(29, 29, 29)"};
   position: absolute;
   border-radius: 1px;
   content: "";
@@ -96,8 +101,8 @@ const Hexagon = styled.div`
   transform: rotate(60deg);
   inset: initial;
   }
-  
-`
+`;
+
 const MouseHighlight = styled.section`
   height: 70vmin;
   width: 70vmin;
@@ -111,7 +116,7 @@ const MouseHighlight = styled.section`
   filter: blur(55px);
   z-index: -105;
   transition: all 0.1s ease;
-`
+`;
 const PinLight = styled.section`
   /* width: 5px;
   height: 5px;
@@ -130,7 +135,7 @@ const PinLight = styled.section`
   border-radius: 50%;
   background-color: #fff;
   box-shadow: 0 0 10px 5px #fff, 0 0 20px 10px #f0f, 0 0 35px 18px #0ff; */
-`
+`;
 
 
 const HexBackground = () => {
@@ -159,14 +164,9 @@ const HexBackground = () => {
     "rgb(37, 37, 37)",
     "transparent",]
     return (
-      <HexagonBox className={`box row${rowKey} col${colKey}`} style={{
-        height: `${HW}px`,
-        width: `${Math.ceil(Dimensions)}px`,
-        inset: "initial",
-        top: `${rowKey * 1.04* HW + colOffset - HW/2}px`,
-        right: `calc(50vw - ${HW *.90  * colKey}px)`,
-        padding: `${HW}`
-      }}>
+      <HexagonBox HW={HW} className={`box row${rowKey} col${colKey}`}
+      topOffset={rowKey * 1.04* HW + colOffset - HW/2}
+      rightOffset={`calc(50vw - ${HW *.90  * colKey}px)`}>
         <HexagonContainer >
           <HexagonInner>
             <Hexagon bgColor={decorative ? `${RandomColor[Math.floor(Math.random() * 4)]}` : "rgb(29, 29, 29)"}/> 
@@ -179,7 +179,6 @@ const HexBackground = () => {
     const dimensions = Math.ceil(Dimensions)
     const columns = Math.floor(window.innerHeight / dimensions)
     const rows = Math.floor(window.innerWidth / dimensions *.60)
-    // console.log(rows)
     const allHexes = []
     const extraHexes = []
     for (let i = rows * -1; i <= rows +1; i++) {
